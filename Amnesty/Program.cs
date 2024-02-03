dd836e775ea2a460a3ebf6053094d218cc886ff0
Макс Крячков <123maxik321@mail.ru>
2024-02-03 18:19:33 +0300

    Добавьте файлы проекта.

---
+++ Amnesty/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Amnesty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dossiersQuantity = 10;
+            string crime = "Антиправительственное";
+
+            CriminalDossierFabrik dossierFabrik = new CriminalDossierFabrik();
+            List<CriminalDossier> criminalDossiers = new List<CriminalDossier>();
+
+            dossierFabrik.GetCrime(crime);
+
+            for (int i = 0; i < dossiersQuantity; i++)
+                criminalDossiers.Add(dossierFabrik.CreateDossier());
+
+            WriteDossierList("до", criminalDossiers);
+
+            criminalDossiers.RemoveAll(dossier => dossier.Crime == crime);
+
+            Console.WriteLine();
+            WriteDossierList("после", criminalDossiers);
+        }
+
+        private static void WriteDossierList(string text, List<CriminalDossier> criminalDossiers)
+        {
+            Console.WriteLine($"Список преступников {text} амнистии:");
+            criminalDossiers.ForEach(dossier => Console.WriteLine($"{dossier.FullName}. Преступление: {dossier.Crime}."));
+        }
+    }
+
+    class CriminalDossier
+    {
+        public CriminalDossier(string fullName, string crime)
+        {
+            FullName = fullName;
+            Crime = crime;
+            IsInJail = true;
+        }
+
+        public string FullName { get; private set; }
+        public string Crime { get; private set; }
+        public bool IsInJail { get; private set; }
+    }
+
+    class CriminalDossierFabrik
+    {
+        private List<string> _names;
+        private List<string> _surnames;
+        private List<string> _crimes;
+
+        private Random _random = new Random();
+
+        public CriminalDossierFabrik()
+        {
+            FillNames();
+            FillSurnames();
+            FillCrimes();
+        }
+
+        public CriminalDossier CreateDossier()
+        {
+            string name = _names[_random.Next(0, _names.Count)];
+            string surname = _surnames[_random.Next(_surnames.Count)];
+            string fullName = $"{name} {surname}";
+
+            string crime = _crimes[_random.Next(0, _crimes.Count)];
+
+            return new CriminalDossier(fullName, crime);
+        }
+
+        private void FillNames() =>
+            _names = new List<string>
+            {
+                "Геннадий",
+                "Дмитрий",
+                "Максим",
+                "Александр",
+                "Валерий",
+                "Михаил"
+            };
+
+        private void FillSurnames() =>
+            _surnames = new List<string>
+            {
+                "Немичев",
+                "Величко",
+                "Андреев",
+                "Кузнецов",
+                "Емельянов",
+                "Киррилов",
+                "Мамонов"
+            };
+
+        private void FillCrimes() =>
+            _crimes = new List<string>
+            {
+                "Коррупция",
+                "Наркоторговля",
+            };
+
+        public void GetCrime(string crime) =>
+            _crimes.Add(crime);
+    }
+}
